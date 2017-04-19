72bfd722eb1bf529694d2e8fe29fc522fddf9451
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2017-04-19 11:46:31 +0200

    copy conf file

--- gammaserver/app.rb
+++ gammaserver/app.rb
@@ -9,18 +9,18 @@ require 'pg'
 
 class GammaApp < Sinatra::Base
   dir = File.dirname(File.expand_path(__FILE__))
-  ms_conf = YAML.load_file("#{dir}/gamma.yaml")
+  gamma_conf = YAML.load_file("#{dir}/../gamma.yaml")
 
   get '/lastsensordata' do
-    puts ms_conf
+    puts gamma_conf
     conn = PGconn.open(
-      :host => ms_conf['gammadb']['host'],
-      :port => ms_conf['gammadb']['port'],
-      :options => ms_conf['gammadb']['options'],
-      :tty =>  ms_conf['gammadb']['tty'],
-      :dbname => ms_conf['gammadb']['dbname'],
-      :user => ms_conf['gammadb']['user'],
-      :password => ms_conf['gammadb']['password']
+      :host => gamma_conf['gammadb']['host'],
+      :port => gamma_conf['gammadb']['port'],
+      :options => gamma_conf['gammadb']['options'],
+      :tty =>  gamma_conf['gammadb']['tty'],
+      :dbname => gamma_conf['gammadb']['dbname'],
+      :user => gamma_conf['gammadb']['user'],
+      :password => gamma_conf['gammadb']['password']
     )
     #WITH latest_measures AS (SELECT id AS theID, max(srv_ts) AS theTS FROM measures GROUP BY id) SELECT * from measures m, latest_measures l WHERE m.id=l.theID AND  m.srv_ts=l.theTS
 
@@ -32,8 +32,8 @@ class GammaApp < Sinatra::Base
     res= conn.exec(
       "SELECT srv_ts, id_device, id_measure, value, max_value, min_value, location, message FROM measures WHERE srv_ts > '#{start_ts}' AND srv_ts <= '#{end_ts}")
 
-    # res= conn.exec("WITH latest_measures AS (SELECT id AS theID, max(srv_ts) AS theTS FROM  #{ms_conf['gammadb']['measurestable']} WHERE temp IS NOT NULL GROUP BY id) " +
-    #   "SELECT * from  #{ms_conf['gammadb']['measurestable']} m, #{ms_conf['gammadb']['sensorparameters']} s, latest_measures l WHERE m.id=l.theID AND  m.srv_ts=l.theTS AND s.id=m.id")
+    # res= conn.exec("WITH latest_measures AS (SELECT id AS theID, max(srv_ts) AS theTS FROM  #{gamma_conf['gammadb']['measurestable']} WHERE temp IS NOT NULL GROUP BY id) " +
+    #   "SELECT * from  #{gamma_conf['gammadb']['measurestable']} m, #{gamma_conf['gammadb']['sensorparameters']} s, latest_measures l WHERE m.id=l.theID AND  m.srv_ts=l.theTS AND s.id=m.id")
     conn.close()
     puts "status " + res.cmd_status().to_s
     puts "tuples " + res.ntuples().to_s
