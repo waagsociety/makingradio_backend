ec0c9907907b658d34089e63564474f5a622b99c
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2017-04-19 13:17:15 +0200

    Deploy passenger server

---
+++ ansible/deploy_server.sh
@@ -0,0 +1,30 @@
+GIT_DIR="${1}"
+REPO="${2}"
+MY_USER=${3}
+
+
+if [ ! -d "${GIT_DIR}" ]
+then
+	sudo su $MY_USER -c "git clone ${REPO} ${GIT_DIR}"
+else
+	if sudo su $MY_USER -c "git -C ${GIT_DIR} remote -v update" | grep master | grep 'origin/master' | grep -v 'up to date' >/dev/null
+	then
+	  echo "Code not changed"
+		exit 0
+	else
+		cd ${GIT_DIR};
+		if ! sudo su $MY_USER -c "git pull"
+		then
+		    echo "ERROR pulling"
+		    exit 1
+		fi
+	fi
+fi
+
+cd ${GIT_DIR}/gammaserver
+
+sudo su $MY_USER -c bundler
+
+sudo passenger stop
+
+sudo passenger start
