a9d95559282311fad2ecfd7a8ab1deafb76b86cb
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2017-10-04 16:13:41 +0200

    Add example commands

---
+++ ansible/playbook.cmd
@@ -0,0 +1,4 @@
+ansible-playbook gammasense.yaml --help
+ansible-playbook gammasense.yaml --syntax-check
+ansible-playbook gammasense.yaml --tags "algo"
+ansible-playbook gammasense.yaml --tags "server"
